8320c57fcc763283338bd7aeda2a01839109d3b8
Natalie Galligan <nat.galligan@gmail.com>
2019-11-08 19:21:26 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -11,5 +11,7 @@ def add_character(show, name)
   BASE_HOA[:chipmunks] << "Dave"
   BASE_HOA[:third_earthers] << "Snarf"
   BASE_HOA[:jetsons] << "Astro"
-  BASE_HOA
+  BASE_HOA[:chipmunks]
+  BASE_HOA[:third_earthers]
+  BASE_HOA[:jetsons]
 end
